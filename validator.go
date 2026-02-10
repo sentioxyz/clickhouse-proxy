7a1e67be0dbcd65b010819338fcb2306ee86250f
mengyu <barmbamos@gmail.com>
2026-02-11 01:01:51 +0800

    revert R8-2: 保留完整 SQL 日志，不截断

--- validator.go
+++ validator.go
@@ -45,12 +45,7 @@ type NoopValidator struct{}
 
 func (NoopValidator) ValidateQuery(_ context.Context, meta QueryMeta) error {
 	if meta.SQL != "" {
-		// R8-2: 截断长 SQL 防止日志爆炸
-		sqlPreview := meta.SQL
-		if len(sqlPreview) > 200 {
-			sqlPreview = sqlPreview[:200] + "..."
-		}
-		log.Infof("[validator] allow query from %s -> %s: %s", meta.ClientAddr, meta.UpstreamAddr, sqlPreview)
+		log.Infof("[validator] allow query from %s -> %s: %s", meta.ClientAddr, meta.UpstreamAddr, meta.SQL)
 	} else if meta.QueryPreview != "" {
 		log.Infof("[validator] allow query (preview) from %s -> %s: %s", meta.ClientAddr, meta.UpstreamAddr, meta.QueryPreview)
 	}
